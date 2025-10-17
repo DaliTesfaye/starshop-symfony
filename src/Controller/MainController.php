bd45efcb324adf124487db566237dc750efbcafd
DaliTesfaye <112868614+DaliTesfaye@users.noreply.github.com>
2025-10-17 10:15:16 +0100

    twig homePage Created

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,10 +4,23 @@ namespace App\Controller ;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-class MainController {
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class MainController extends AbstractController {
     #[Route('/' )]
+
+
     public function homePage() : Response {
-        return new Response ("Hello <strong>World</strong> !") ;
+
+        $starShipsCount = 457;
+        $myShips = [
+            "name" => "Millenium Falcon",
+            "class" => "Light freighter",
+            "captain" => "Han Solo",
+            "status" => "Docked",
+        ];
+        return $this->render('main/homePage.html.twig' , [
+            'numberOfShips' => $starShipsCount,
+            'ships' => $myShips,
+        ]);
     }
 }
